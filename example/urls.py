ce743a51543b72dc1948ce9550d1a39bbd93050c
bokdol11859 <2019147551@yonsei.ac.kr>
2022-07-12 01:29:07 +0900

    Mixin, Generics, Viewsets, Router

--- example/urls.py
+++ example/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
+from rest_framework import routers
 
-from example.views import booksAPI, bookAPI, BooksAPI, BookAPI
+from example.views import booksAPI, bookAPI, BooksAPI, BookAPI, BooksAPIMixins, BookAPIMixins, BooksAPIGenerics, \
+    BookAPIGenerics, BookViewSet
 
-urlpatterns = [
-    path('fbv/books/', booksAPI),
-    path('fbv/book/<int:bid>/', bookAPI),
-    path('cbv/books/', BooksAPI.as_view()),
-    path('cbv/book/<int:bid>/', BookAPI.as_view())
-]
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+# urlpatterns = [
+#     path('fbv/books/', booksAPI),
+#     path('fbv/book/<int:bid>/', bookAPI),
+#     path('cbv/books/', BooksAPI.as_view()),
+#     path('cbv/book/<int:bid>/', BookAPI.as_view()),
+#     path('mixin/books/', BooksAPIMixins.as_view()),
+#     path('mixin/book/<int:bid>/', BookAPIMixins.as_view()),
+#     path('generics/books/', BooksAPIGenerics.as_view()),
+#     path('generics/book/<int:bid>/', BookAPIGenerics.as_view()),
+#
+# ]
+
+urlpatterns = router.urls
